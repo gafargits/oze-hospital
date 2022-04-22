ce56ae0a4b2ad4101664db0f2423fc676aef16cc
gafargits <rajigafaronline@gmail.com>
2022-04-22 17:11:16 +0100

    clean up

--- README.md
+++ README.md
@@ -10,18 +10,19 @@
 The service is a set of REST API endpoints that allows for the user to 
 - register as a staff
 - login as a staff
-- validated staff update own record as a staff
-- validated staff get records patients (up to two year old records).
+- validated staff can update own record
+- validated staff can get records patients (up to two year old records).
 - validated staff can get record of specific patient in a csv format
 - validated staff can delete multiple records within a provided time range.
 
 ***A validated staff is a staff that must have been registered and consequently logged in at the time they are about using the system.***
 
 ### Constraints and Assumptions
-- Staff are required to register and log in before they can have access to some of the protected endpoints.
+- A staff is required to register and log in before they can have access to some of the protected endpoints.
 - The login requires the staff name and password that must have been provided at the point of registration.
+- It was assumed while designing the application that the name of each staff will be unique within the application. 
 - A staff can only update his/her own record. 
-- As stated in the requirement, when fetching records of patients, only patients with last visit date within the last two (2) years are fetched.
+- As stated in the requirement document, when fetching records of all patients, only patients with last visit date within the last two (2) years are returned.
 
 ### Starting Application
 The application has been packaged in a docker container. 
--- src/test/java/com/oze/hospitalservice/HospitalServiceApplicationTests.java
+++ src/test/java/com/oze/hospitalservice/HospitalServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.oze.hospitalservice;
 
+import com.oze.hospitalservice.exceptions.InvalidRequestException;
 import com.oze.hospitalservice.repositories.StaffRepository;
 import com.oze.hospitalservice.services.StaffService;
 import com.oze.hospitalservice.services.StaffServiceImpl;
@@ -33,26 +34,32 @@ class HospitalServiceApplicationTests extends BaseServiceTest {
 	@Test
 	@DisplayName("register_staff_successful")
 	void registerStaff_successful() {
-		//Given
 		String name = "gafar";
 		String password = "password";
 		var registeredStaff = getRegisteredStaff(name, password);
 		Map<String, Object> generatedKeys = Map.of("id", registeredStaff.getId(), "staff_id", registeredStaff.getStaffId(),"registration_date", registeredStaff.getRegistrationDate());
 
-		//When
 		when(staffRepository.create(eq(name), anyString())).thenReturn(generatedKeys);
 		var response = staffService.registerStaff(name, password);
-		//Then
+
 		assertNotNull(response);
 		assertThat(response).usingRecursiveComparison().isEqualTo(registeredStaff);
 	}
 
 	@Test
-	void registerStaff_staffAlreadExist_throwsException() {
+	@DisplayName("register_staff_ensure_is_unique")
+	void registerStaff_ensureUserIsUnique() {
 		//Given
-		String name = "";
-		String password = "";
-		var registerStaff = getRegisteredStaff(name, password);
+		String name = "gafar";
+		String password = "password";
+		var registeredStaff = registerStaff(name, password);
+		Map<String, Object> generatedKeys = Map.of("id", registeredStaff.getId(), "staff_id", registeredStaff.getStaffId(),"registration_date", registeredStaff.getRegistrationDate());
+
+		when(staffRepository.create(eq(name), anyString())).thenReturn(generatedKeys);
+		var response = staffService.registerStaff(name, password);
+
+		verify(staffRepository, times(1)).getUserCountByName(name);
+		assertNotNull(response);
 
 	}
 
